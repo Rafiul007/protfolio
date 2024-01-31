2a629c68ead009431a9fb7f7ed0ee9849f2d4ece
Rafiul007 <rafiul.faisal@gmail.com>
2024-01-31 13:02:48 +0600

    hero section done:

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import hero from '../../Assests/me.jpg'
 function Home() {
     return (
         <>
@@ -13,6 +14,29 @@ function Home() {
                     </ul>
                 </div>
                 {/* Hero section */}
+                <div className="hero-container">
+                    <div className="hero-left">
+                        <h1>Hi, I am <span>Rafiul Faisal</span>,<br />Fullstack Developer</h1>
+                        <p>A fullstack developer, who turning ideas into real life product.<br />Looking for fulltime opportunity.</p>
+                        <button>Download Resume</button>
+                    </div>
+                    <div className="hero-right">
+                        <div className="hero-img-container">
+                            <img src={hero} alt="Rafiul Faisal" />
+                        </div>
+                    </div>
+                </div>
+                {/* Services Section */}
+                <div className="services-container">
+                    <div className="services-sub-container">
+                        <div className="service-heading">
+                            <h1>What I Do?</h1>
+                        </div>
+                        <div className="service-cards">
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
