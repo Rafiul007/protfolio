1b18887bf75158b0f896f8e5124a1978f937cee8
Rafiul007 <rafiul.faisal@gmail.com>
2024-03-21 02:58:17 +0600

    download resume added

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -6,8 +6,14 @@ import WebIcon from '@mui/icons-material/Web';
 import WebhookOutlinedIcon from '@mui/icons-material/WebhookOutlined';
 import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
 import Progressbar from '../../Components/Progressbar/Progressbar'
-import {Typewriter } from "react-simple-typewriter"
+import { Typewriter } from "react-simple-typewriter"
+import Aos from 'aos'
+import ResumePDF from '../../Assests/Rafiul Faisal.pdf';
 function Home() {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+
+    }, [])
     const data = [
         {
             title: "Web Development",
@@ -43,22 +49,30 @@ function Home() {
         },
         {
             name: "ReactJS",
-            percentage: 86,
+            percentage: 85,
         },
         {
             name: "NodeJS",
-            percentage: 76,
+            percentage: 78,
         },
         {
             name: "ExpressJS",
-            percentage: 82,
+            percentage: 90,
         },
         {
             name: "PHP",
-            percentage: 62,
+            percentage: 65,
         }
     ]
-    
+
+    const handleDownload = () => {
+        const resumePath = ResumePDF;
+        const anchor = document.createElement('a');
+        anchor.href = resumePath;
+        anchor.download = 'rafiul faisal-resume.pdf';
+        anchor.click();
+      };
+
     return (
         <>
             <div className="home-section">
@@ -73,20 +87,20 @@ function Home() {
                 {/* Hero section */}
                 <div className="hero-container">
                     <div className="hero-left">
-                        <h1>Hi, I am <span>Rafiul Faisal</span>,<br /><span>{<Typewriter words={['Fullstack Developer', 'Backend Developer', 'React JS Developer']} loop={100}  />}</span></h1>
+                        <h1>Hi, I am <span>Rafiul Faisal</span>,<br /><span>{<Typewriter words={['Fullstack Developer', 'Backend Developer', 'React JS Developer']} loop={100} />}</span></h1>
                         <p>A fullstack developer, who turning ideas into real life product.<br />Looking for fulltime opportunity.</p>
-                        <button>Download Resume</button>
+                        <button onClick={handleDownload}>Download Resume</button>
                     </div>
                     <div className="hero-right">
                         <div className="hero-img-container">
-                            <img src={hero} alt="Rafiul Faisal" />
+                            <img src={hero} alt="Rafiul Faisal"  />
                         </div>
                     </div>
                 </div>
                 {/* Services Section */}
                 <div className="services-container">
                     <div className="services-sub-container">
-                        <div className="service-heading">
+                        <div className="service-heading" data-aos="zoom-out-left">
                             <h1>What I Do?</h1>
                         </div>
                         <div className="service-cards">
